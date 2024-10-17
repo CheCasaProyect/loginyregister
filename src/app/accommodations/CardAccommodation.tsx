714f482a65e8c987c5ed6f09586470f2c2af275a
RocioMigliarese <rociomigliarese3@gmail.com>
2024-10-17 09:11:56 -0300

    feature (app/accommodations y app/utilities) create component

---
+++ src/app/accommodations/CardAccommodation.tsx
@@ -0,0 +1,20 @@
+interface IAccommodation {
+    id?: number;  
+    title: string;
+    description: string;
+    price: number;
+    image: string;
+  }
+  
+  const CardAccommodation: React.FC<IAccommodation> = ({ title, description, price, image }) => {
+    return (
+      <div className="card bg-white shadow-lg p-4 rounded-lg">
+        <img src={image} alt={title} className="w-full h-40 object-cover rounded-md" />
+        <h2 className="text-lg font-bold mt-2">{title}</h2>
+        <p className="text-sm text-gray-500">{description}</p>
+        <p className="text-lg font-semibold text-blue-600">${price} por noche</p>
+      </div>
+    );
+  };
+  
+  export default CardAccommodation;
---
+++ src/app/utilities/accommodations.tsx
@@ -0,0 +1,23 @@
+export const accommodations = [
+    {
+      id: 1,
+      title: 'Hermosa casa en la playa',
+      description: 'Disfruta de una increíble vista al mar.',
+      price: 120,
+      image: 'https://a0.muscache.com/im/pictures/73708697-309d-496b-b1c5-641fc0ed64e8.jpg?im_w=1200',
+    },
+    {
+      id: 2,
+      title: 'Apartamento moderno en el centro',
+      description: 'Cerca de todo, ideal para turismo.',
+      price: 90,
+      image: 'https://a0.muscache.com/im/ml/photo_enhancement/pictures/49057231-43b6-4dde-aa95-90b2767ca749.jpg?im_w=1200',
+    },
+    {
+      id: 3,
+      title: 'Cabaña en las montañas',
+      description: 'Desconecta y relájate rodeado de naturaleza.',
+      price: 150,
+      image: 'https://a0.muscache.com/im/pictures/miso/Hosting-904491728479980863/original/433f7179-e990-4888-b849-75d4a6fbbe34.jpeg?im_w=1200',
+    },
+  ];
