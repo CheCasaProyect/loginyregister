808ca86e2ef8e60febf1c5230940364a36e1028e
FrancoVeliz <francoveliz018@gmail.com>
2024-10-22 11:26:36 -0300

    pusheo 22/10

--- src/app/accommodations/CardAccommodation.tsx
+++ src/app/accommodations/CardAccommodation.tsx
@@ -4,7 +4,7 @@ import IAccommodation from "@/interfaces/Accomodation"
     return (
       <div className="card bg-white shadow-lg p-4 rounded-lg">
         <img src={image} alt={title} className="w-full h-40 object-cover rounded-md" />
-        <h2 className="text-lg font-bold mt-2">{title}</h2>
+        <h2 className="text-lg  text-gray-500 font-bold mt-2">{title}</h2>
         <p className="text-sm text-gray-500">{description}</p>
         <p className="text-lg font-semibold text-blue-600">${price} por noche</p>
       </div>
