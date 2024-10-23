3afdf052e0ceef8ac24ba3ee3533220d7c58ea79
RocioMigliarese <rociomigliarese3@gmail.com>
2024-10-22 23:31:01 -0300

    fix (app/login y app/register) correccion de errores img y responsive

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,9 +1,10 @@
-"use client";
+"use client"
 import { Formik, Form, Field, ErrorMessage } from "formik";
 import { useRouter } from "next/navigation";
 import * as Yup from "yup";
 import { useState } from "react";
 import Image from 'next/image';
+import Swal from 'sweetalert2'; 
 
 const Login = () => {
   const [error, setError] = useState("");
@@ -41,6 +42,11 @@ const Login = () => {
       if (!response.ok) {
         if (response.status === 401) {
           setError("Credenciales incorrectas. Intentá de nuevo");
+          Swal.fire({
+            icon: 'error',
+            title: 'Oops...',
+            text: 'Credenciales incorrectas. Intentá de nuevo',
+          });
         } else {
           throw new Error("Error al iniciar sesión. Intentá de nuevo.");
         }
@@ -53,15 +59,35 @@ const Login = () => {
       localStorage.setItem("token", data.token);
       localStorage.setItem("user", JSON.stringify(userData));
 
-      setSuccessMessage("Incio de sesión exitoso!");
+      setSuccessMessage("Inicio de sesión exitoso!");
       setError("");
 
-      router.push("/profile"); //decidir a donde dirigir cuando se loguea
+      Swal.fire({
+        icon: 'success',
+        title: 'Inicio de sesión exitoso!',
+        text: 'Serás redirigido al perfil.',
+        timer: 2000,
+        timerProgressBar: true,
+        willClose: () => {
+          router.push("/profile");
+        }
+      });
+
     } catch (error) {
       if (error instanceof Error) {
         setError(error.message);
+        Swal.fire({
+          icon: 'error',
+          title: 'Oops...',
+          text: error.message,
+        });
       } else {
         setError("Un error desconocido ocurrió.");
+        Swal.fire({
+          icon: 'error',
+          title: 'Oops...',
+          text: 'Un error desconocido ocurrió.',
+        });
       }
     } finally {
       setSubmitting(false); 
@@ -69,87 +95,80 @@ const Login = () => {
   };
 
   return (
-    <div className="flex items-center justify-between min-h-screen bg-[#fffefe] text-[#0a0a0a]">
-      <div className="flex-1 flex items-center justify-center max-w-md px-10">
-        <Image
-          src="/image/carpi.svg"
-          alt="Carpi Bienvenida"
-          width={256}
-          height={256}
-          className="object-cover mr-4" 
-        />
-        <div>
-          <h2 className="text-6xl font-bold mb-4">Che! Volviste</h2>
-          <p className="leading-relaxed mb-6">
-            Bienvenido a la experiencia del turismo argentino. Desde la
-            majestuosidad de la Patagonia hasta las vibrantes ciudades, estamos
-            aquí para ayudarte a planificar tu próxima aventura.
-          </p>
-        </div>
-      </div>
-      <div className="flex justify-center items-center w-full max-w-lg p-8 bg-white bg-opacity-90 border border-[#0a0a0a] rounded-md shadow-lg space-y-6 mr-20">
-        <div className="w-full">
-          <h2 className="text-2xl font-bold text-center tracking-wider">
-            Inicia sesión
-          </h2>
-
-          {error && <p className="text-red-500 text-center">{error}</p>}
-          {successMessage && (
-            <p className="text-green-500 text-center">{successMessage}</p>
-          )}
-
-          <Formik
-            initialValues={initialValues}
-            validationSchema={validationSchema}
-            onSubmit={handleSubmit}
-          >
-            {({ isSubmitting }) => (
-              <Form className="space-y-6">
-                <div className="relative">
-                  <label className="sr-only">Email</label>
-                  <Field
-                    type="email"
-                    name="email"
-                    className="w-full bg-transparent border-b border-[#0a0a0a] text-lg text-[#0a0a0a] placeholder-gray-500 focus:outline-none p-2"
-                    placeholder="Email"
-                  />
-                  <ErrorMessage
-                    name="email"
-                    component="div"
-                    className="text-red-500"
-                  />
-                </div>
-
-                <div className="relative">
-                  <label className="sr-only">Contraseña</label>
-                  <Field
-                    type="password"
-                    name="password"
-                    className="w-full bg-transparent border-b border-[#0a0a0a] text-lg text-[#0a0a0a] placeholder-gray-500 focus:outline-none p-2"
-                    placeholder="Contraseña"
-                  />
-                  <ErrorMessage
-                    name="password"
-                    component="div"
-                    className="text-red-500"
-                  />
-                </div>
-
-                <div className="flex justify-between mt-6 space-x-4">
-                  <button
-                    type="submit"
-                    className="flex-1 border border-[#0a0a0a] text-[#0a0a0a] text-sm py-2 rounded-md hover:bg-gray-300 transition duration-300"
-                    disabled={isSubmitting}
-                  >
-                    {isSubmitting ? "Iniciando sesión..." : "Inicia Sesión"}
-                  </button>
-                </div>
-              </Form>
-            )}
-          </Formik>
-        </div>
-      </div>
+    <div className="flex flex-col lg:flex-row items-center justify-center min-h-screen bg-[#fffefe] text-[#0a0a0a] pt-20 lg:pt-40 pb-20">
+  <div className="flex-1 flex flex-col lg:flex-row items-center justify-center max-w-2xl px-5 sm:px-10 mb-10 lg:mb-0">
+    <Image
+      src="https://i.postimg.cc/G3QXSw8Y/carpincho.png"
+      alt="Carpi Bienvenida"
+      width={300}
+      height={300}
+      className="object-cover w-48 h-48 sm:w-64 sm:h-64 mb-6 lg:mb-0 lg:mr-8"
+    />
+    <div className="text-center lg:text-left">
+      <h2 className="text-3xl sm:text-4xl lg:text-6xl font-bold mb-4">
+        Che! Volviste
+      </h2>
+      <p className="leading-relaxed text-base sm:text-lg lg:text-xl mb-6">
+        Bienvenido a la experiencia del turismo argentino. Desde la majestuosidad de la Patagonia hasta las vibrantes ciudades, estamos aquí para ayudarte a planificar tu próxima aventura.
+      </p>
     </div>
+  </div>
+  <div className="w-full max-w-lg p-6 sm:p-8 bg-white bg-opacity-90 border border-[#0a0a0a] rounded-md shadow-lg space-y-6 lg:ml-20">
+    <h2 className="text-xl sm:text-2xl font-bold text-center tracking-wider">
+      Inicia sesión
+    </h2>
+
+    <Formik
+      initialValues={initialValues}
+      validationSchema={validationSchema}
+      onSubmit={handleSubmit}
+    >
+      {({ isSubmitting }) => (
+        <Form className="space-y-6">
+          <div className="relative">
+            <label className="sr-only">Email</label>
+            <Field
+              type="email"
+              name="email"
+              className="w-full bg-transparent border-b border-[#0a0a0a] text-lg text-[#0a0a0a] placeholder-gray-500 focus:outline-none p-2"
+              placeholder="Email"
+            />
+            <ErrorMessage
+              name="email"
+              component="div"
+              className="text-red-500"
+            />
+          </div>
+
+          <div className="relative">
+            <label className="sr-only">Contraseña</label>
+            <Field
+              type="password"
+              name="password"
+              className="w-full bg-transparent border-b border-[#0a0a0a] text-lg text-[#0a0a0a] placeholder-gray-500 focus:outline-none p-2"
+              placeholder="Contraseña"
+            />
+            <ErrorMessage
+              name="password"
+              component="div"
+              className="text-red-500"
+            />
+          </div>
+
+          <div className="flex justify-between mt-6 space-x-4">
+            <button
+              type="submit"
+              className="flex-1 border border-[#0a0a0a] text-[#0a0a0a] text-sm py-2 rounded-md hover:bg-gray-300 transition duration-300"
+              disabled={isSubmitting}
+            >
+              {isSubmitting ? "Iniciando sesión..." : "Inicia Sesión"}
+            </button>
+          </div>
+        </Form>
+      )}
+    </Formik>
+  </div>
+</div>
   );
 };
 
