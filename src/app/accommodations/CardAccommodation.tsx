bbee6afca445b468561614e2b6b16762ef3c4d9d
camimontechiani <cmontechiani2@gmail.com>
2024-10-25 14:52:56 -0300

    fixing/accomodations[id]

--- src/app/accommodations/CardAccommodation.tsx
+++ src/app/accommodations/CardAccommodation.tsx
@@ -1,24 +1,40 @@
-"use client"
+"use client";
 import { useRouter } from 'next/navigation';
 import IAccommodation from "@/interfaces/Accomodation";
+import React from 'react';
 
 const CardAccommodation: React.FC<IAccommodation> = ({ id, title, description, price, image }) => {
   const router = useRouter();
 
-  // Función para manejar el clic
   const handleCardClick = () => {
-    router.push(`/accommodations/${id}`); 
+    router.push(`/accommodations/${id}`);
   };
 
   return (
-    <div 
-      className="card bg-white shadow-lg p-4 rounded-lg cursor-pointer" 
-      onClick={handleCardClick} 
+    <div
+      className="relative overflow-hidden w-60 h-80 rounded-3xl cursor-pointer shadow-lg"
+      onClick={handleCardClick}
     >
-      <img src={image} alt={title} className="w-full h-40 object-cover rounded-md" />
-      <h2 className="text-lg text-black font-bold mt-2">{title}</h2>
-      <p className="text-sm text-black">{description}</p>
-      <p className="text-lg font-semibold text-blue-600">${price} por noche</p>
+      {/* Imagen ocupando todo el espacio de la carta */}
+      <img className="object-cover h-full w-full rounded-3xl" src={image} alt={title} />
+
+      {/* Efecto de hover */}
+      <div className="absolute inset-0 z-10 flex items-center justify-center transition-all duration-500 peer"></div>
+      <div
+        className="absolute inset-0 flex flex-col items-center justify-center transition-all duration-500 opacity-0 peer-hover:opacity-100 bg-black bg-opacity-70 rounded-3xl"
+      >
+        <h2 className="font-bold text-2xl text-white text-center">{title}</h2>
+        <p className="text-lg text-white text-center">{description}</p>
+        <span className="inline-block rounded-full px-3 py-1 text-sm font-semibold text-white"> {/* Sin fondo azul */}
+          ${price} por noche
+        </span>
+      </div>
+
+      {/* Contenedor de texto (oculto al hacer hover) */}
+      <div className="px-6 py-4 h-28 flex flex-col justify-between items-center transition-opacity duration-500 opacity-100 peer-hover:opacity-0">
+        <h2 className="font-bold text-xl mb-2 text-center">{title}</h2>
+        <p className="text-gray-700 text-base text-center">{description}</p>
+      </div>
     </div>
   );
 };
