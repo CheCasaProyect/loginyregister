1ebc4cea8e27b37d8341744d7e00bdc4a7060d00
RocioMigliarese <rociomigliarese3@gmail.com>
2024-10-27 19:27:43 -0300

    fix(app/store

--- src/store/authStore.ts
+++ src/store/authStore.ts
@@ -1,10 +1,5 @@
 import { create } from "zustand";
-<<<<<<< Updated upstream:src/store/authStore.ts
 import IAuthState from "../interfaces/Auth"
-=======
-import { persist} from "zustand/middleware";
-import IAuthState from "../../interfaces/Auth"
->>>>>>> Stashed changes:src/app/store/authStore.ts
 
 export const useAuthStore = create<IAuthState>((set) => ({
   token: null,
