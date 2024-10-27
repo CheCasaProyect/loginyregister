ed9267a63ab657b1b558133e0b0ff6bf5c38c0bf
RocioMigliarese <rociomigliarese3@gmail.com>
2024-10-27 19:23:25 -0300

    feature(app/store) fix

--- src/store/authStore.ts
+++ src/store/authStore.ts
@@ -1,5 +1,10 @@
 import { create } from "zustand";
+<<<<<<< Updated upstream:src/store/authStore.ts
 import IAuthState from "../interfaces/Auth"
+=======
+import { persist} from "zustand/middleware";
+import IAuthState from "../../interfaces/Auth"
+>>>>>>> Stashed changes:src/app/store/authStore.ts
 
 export const useAuthStore = create<IAuthState>((set) => ({
   token: null,
@@ -12,3 +17,9 @@ export const useAuthStore = create<IAuthState>((set) => ({
   setSuccessMessage: (message) => set({ successMessage: message }),
   clearMessages: () => set({ error: "", successMessage: "" }),
 }));
+
+
+
+
+
+
