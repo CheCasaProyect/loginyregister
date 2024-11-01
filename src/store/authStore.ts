bb4e8d7f5d44fe09aeb75441c0d0e6003e89f4cc
RocioMigliarese <rociomigliarese3@gmail.com>
2024-11-01 00:38:17 -0300

    feture(app/userDashboar) renderizado de info user logueado

--- src/store/authStore.ts
+++ src/store/authStore.ts
@@ -1,11 +1,13 @@
 import { create } from "zustand";
 import IAuthState from "../interfaces/Auth"
+import IUser from "@/interfaces/Iuser";
 
 export const useAuthStore = create<IAuthState>((set) => ({
   token: null,
-  user: null,
+  user: null as IUser | null,
   error: "",
   successMessage: "",
+
   setToken: (token) => set({ token }),
   setUser: (user) => set({ user }),
   setError: (error) => set({ error }),
@@ -18,4 +20,29 @@ export const useAuthStore = create<IAuthState>((set) => ({
     error: "",
     successMessage: "",
   }),
+
+  loginUser: async (email: string, password: string) => {
+    try {
+      const response = await fetch("https://proyectochecasa.onrender.com/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ email, password }),
+      });
+
+      if (!response.ok) {
+        throw new Error("Credenciales incorrectas.");
+      }
+
+      const data = await response.json();
+      const { token } = data;
+
+      set({ token, error: "", successMessage: "Inicio de sesión exitoso" });
+
+      localStorage.setItem("token", token);
+    } catch (error: any) {
+      set({ error: error.message || "Error en el inicio de sesión", successMessage: "" });
+    }
+  },
 }));
