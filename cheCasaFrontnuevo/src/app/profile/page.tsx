3a02b9ffc4472af3f8f089bebd2f24a6dede51e5
RocioMigliarese <rociomigliarese3@gmail.com>
2024-11-06 02:03:11 -0300

    refactorizacion 10 millones

--- cheCasaFrontnuevo/src/app/profile/page.tsx
+++
@@ -1,9 +0,0 @@
-"use client"
-import React from 'react';
-import UserDashboard from '../../components/UserDashboard/UserDashboard';
-
-const ProfilePage: React.FC = () => {
-  return <UserDashboard />;
-};
-
-export default ProfilePage;
